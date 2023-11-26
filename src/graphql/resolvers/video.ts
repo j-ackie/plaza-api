f3bec618754b2dbaa549416cf331ecf8a5ab7734
Jackie Lin <jackielin2025@u.northwestern.edu>
2023-11-25 22:28:26 -0600

    video upload

--- src/graphql/resolvers/video.ts
+++ src/graphql/resolvers/video.ts
@@ -1,10 +1,7 @@
 import connection from '../../db';
 import client from '../../s3';
-import { PutObjectCommand } from "@aws-sdk/client-s3";
-import {
-  getSignedUrl,
-  S3RequestPresigner,
-} from "@aws-sdk/s3-request-presigner";
+import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import crypto from 'crypto';
 
 import Ffmpeg = require('fluent-ffmpeg');
@@ -12,119 +9,122 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 Ffmpeg.setFfmpegPath(ffmpegPath);
 
 type VideoCreateInput = {
-    video: {
-        videoURL: string
+  video: {
+    videoURL: string;
 
-        description: string
-        
-        productIDs: [number]
-    };
+    description: string;
+
+    productIDs: [number];
   };
+};
 
 type Video = {
-  id: number
+  id: number;
 
-  user_id: number
+  user_id: number;
 
-  video_url: string
+  video_url: string;
 
-  description: string
-}
-  
+  description: string;
+};
+
 const videoQueries = {
-    video: async (_: any, args: any) => {
-      const video = await connection('Video')
-        .select('*')
-        .where('id', args.videoID)
+  video: async (_: any, args: any) => {
+    const video = (
+      await connection('Video').select('*').where('id', args.videoID)
+    )[0];
 
-      console.log(video)
-      if(!video){
-        return null
-      }
+    const products = await connection('Product')
+      .select('*')
+      .join('ProductImage', 'ProductImage.product_id', '=', 'Product.id')
+      .join('VideoProduct', 'Product.id', '=', 'VideoProduct.product_id')
+      .where('VideoProduct.video_id', video.id);
 
-      return video.map(async video => {
-        const products = await connection("Product")
-          .select('*')
-          .join("ProductImage", "ProductImage.product_id", "=", "Product.id")
-          .join("VideoProduct", "Product.id", "=", "VideoProduct.product_id")
-          .where("VideoProduct.video_id", video.id)
+    const command = new GetObjectCommand({
+      Bucket: 'plaza-videos-images',
+      Key: video.video_bucket_key,
+    });
+    const presignedUrl = getSignedUrl(client, command, { expiresIn: 3600 });
 
-        return ({
-          id: video.id,
-          userID: video.user_id,
-          videoURL: video.video_url,
-          description: video.description,
-          products: products.map(product => ({
-              id: product.id,
-              sellerID: product.seller_id,
-              name: product.name,
-              description: product.description,
-              quantity: product.quantity,
-              price: product.price,
-              imageURI: product.image_uri
-          }))
-        })
-      })
-    },
-    videos: async (_: any, args: any, ctx: any) => {
+    return {
+      id: video.id,
+      userID: video.user_id,
+      videoURL: presignedUrl,
+      description: video.description,
+      products: products.map((product) => ({
+        id: product.id,
+        sellerID: product.seller_id,
+        name: product.name,
+        description: product.description,
+        quantity: product.quantity,
+        price: product.price,
+        imageURI: product.image_uri,
+      })),
+    };
+  },
+  videos: async (_: any, args: any, ctx: any) => {
+    const videos = await connection('Video')
+      .select('*')
+      .where('user_id', args.userID);
 
-      const videos = await connection('Video')
-        .select('*')
-        .where('user_id', args.userID);
-  
-      // if (ctx.user.id != resource.owner.id) {
-      //   return null;
-      // }
-      
-      return videos.map(async (video) => {
-        
-        return {
-          id: video.id,
-          userID: video.user_id,
+    // if (ctx.user.id != resource.owner.id) {
+    //   return null;
+    // }
 
-        }
-      })
-    },
-  };
-
-  const videoMutations = {
-    createVideo: async (parent: undefined, args: VideoCreateInput, ctx: any) => {
-      // Ffmpeg(args.video.videoURL)
-      // .on('end', function() {
-      //   console.log('Screenshots taken');
-      // })
-      // .on('error', function(err) {
-      //   console.error(err);
-      // })
-      // .screenshots({
-      //   // Will take screenshots at 20%, 40%, 60% and 80% of the video
-      //   count: 4,
-      //   folder: './asset/output',
-      //   filename: "output.jpg",
-      //   size: '320x240',
-      //   timemarks: [ '1' ]
-      // });
-      console.log(args);
-
-      const bucketKey = crypto.randomBytes(32).toString('hex');
-      const command = new PutObjectCommand({ Bucket: 'plaza-videos-images', Key: bucketKey })
-      const presignedUrl = getSignedUrl(client, command, { expiresIn: 60 });
-      
-      const insertedObject = (await connection("Video").insert({
-        user_id: ctx.user.id,
-        video_bucket_key: bucketKey,
-        description: args.video.description,
-      }).returning('*'))[0];
-
-      // Insert VideoProducts
+    return videos.map(async (video) => {
       return {
-        id: insertedObject.id,
-        userID: insertedObject.user_id,
-        videoURL: presignedUrl,
-        description: insertedObject.description,
-        products: []
+        id: video.id,
+        userID: video.user_id,
       };
-    }
-  }
+    });
+  },
+};
 
-  export {videoQueries, videoMutations}
+const videoMutations = {
+  createVideo: async (parent: undefined, args: VideoCreateInput, ctx: any) => {
+    // Ffmpeg(args.video.videoURL)
+    // .on('end', function() {
+    //   console.log('Screenshots taken');
+    // })
+    // .on('error', function(err) {
+    //   console.error(err);
+    // })
+    // .screenshots({
+    //   // Will take screenshots at 20%, 40%, 60% and 80% of the video
+    //   count: 4,
+    //   folder: './asset/output',
+    //   filename: "output.jpg",
+    //   size: '320x240',
+    //   timemarks: [ '1' ]
+    // });
+    console.log(args);
+
+    const bucketKey = crypto.randomBytes(32).toString('hex');
+    const command = new PutObjectCommand({
+      Bucket: 'plaza-videos-images',
+      Key: bucketKey,
+    });
+    const presignedUrl = getSignedUrl(client, command, { expiresIn: 60 });
+
+    const insertedObject = (
+      await connection('Video')
+        .insert({
+          user_id: ctx.user.id,
+          video_bucket_key: bucketKey,
+          description: args.video.description,
+        })
+        .returning('*')
+    )[0];
+
+    // Insert VideoProducts
+    return {
+      id: insertedObject.id,
+      userID: insertedObject.user_id,
+      videoURL: presignedUrl,
+      description: insertedObject.description,
+      products: [],
+    };
+  },
+};
+
+export { videoQueries, videoMutations };
