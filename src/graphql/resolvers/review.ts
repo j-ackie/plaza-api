fb38300e72cd3cccea5b9a3adf9f9e5c71133901
Jackie Lin <jackielin2025@u.northwestern.edu>
2023-11-27 20:15:29 -0600

    post reviews

--- src/graphql/resolvers/review.ts
+++ src/graphql/resolvers/review.ts
@@ -1,3 +1,4 @@
+import { GraphQLError } from 'graphql';
 import connection from '../../db';
 
 type ReviewCreateInput = {
@@ -9,47 +10,68 @@ type ReviewCreateInput = {
   };
 };
 
-type ReviewFilters =  {
+type ReviewFilters = {
   filters: {
     productID: number | null;
     sellerID: number | null;
-  }
-}
+  };
+};
 
 // limit reviews later
 const reviewQueries = {
   reviews: async (_: any, args: ReviewFilters) => {
-    const reviews = await connection("Review")
-      .select("*")
-      .where(!!args.filters.productID ? {product_id: args.filters.productID} : {seller_id: args.filters.sellerID});
+    if (!args.filters.productID && !args.filters.sellerID) {
+      throw new GraphQLError(
+        'Either productID or sellerID has to be provided',
+        {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        }
+      );
+    }
 
-    return reviews.map(review => ({
+    const reviews = await connection('Review')
+      .select('Review.*')
+      .where(
+        !!args.filters.productID
+          ? { product_id: args.filters.productID }
+          : { seller_id: args.filters.sellerID }
+      )
+      .join('Product', 'Product.id', '=', 'Review.product_id');
+
+    return reviews.map((review) => ({
       id: review.id,
       productID: review.product_id,
       reviewerID: review.reviewer_id,
       title: review.title,
       description: review.description,
       rating: review.rating,
-      createdAt: review.created_at
+      createdAt: review.created_at,
     }));
-    
-}};
-  
+  },
+};
 
 const reviewMutations = {
-  createReview: async (parent: undefined, args: ReviewCreateInput, ctx: any) => {
+  createReview: async (
+    parent: undefined,
+    args: ReviewCreateInput,
+    ctx: any
+  ) => {
     console.log(ctx);
-    
-    const insertedObject = (await connection("Review").insert({
-      product_id: args.review.productID,
-      reviewer_id: ctx.user.id,
-      title: args.review.title,
-      description: args.review.description,
-      rating: args.review.rating,
-      created_at: new Date().toISOString(),
-    }).returning('*'))[0];
 
-
+    const insertedObject = (
+      await connection('Review')
+        .insert({
+          product_id: args.review.productID,
+          reviewer_id: ctx.user.id,
+          title: args.review.title,
+          description: args.review.description,
+          rating: args.review.rating,
+          created_at: new Date().toISOString(),
+        })
+        .returning('*')
+    )[0];
 
     return {
       id: insertedObject.id,
@@ -58,9 +80,9 @@ const reviewMutations = {
       title: insertedObject.title,
       description: insertedObject.description,
       rating: insertedObject.rating,
-      createdAt: insertedObject.created_at
+      createdAt: insertedObject.created_at,
     };
-  }
-}
+  },
+};
 
 export { reviewQueries, reviewMutations };
